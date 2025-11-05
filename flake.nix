40eb83c305e5d91bb79fa19a196e6b6fa0a123b3
Yusong <yusonglai64@gmail.com>
2025-11-06 07:53:58 +0800

    chore: set up dev environment by nix flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A Nix-flake-based development environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs =
+    inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system: f { pkgs = import inputs.nixpkgs { inherit system; }; }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            # The Nix packages installed in the dev environment.
+            packages = with pkgs; [
+              typos # check misspelling
+            ];
+            # The shell script executed when the environment is activated.
+            shellHook = ''
+              # Print the last modified date of "flake.lock".
+              stat flake.lock | grep "Modify" |
+                awk '{printf "\"flake.lock\" last modified on: %s", $2}' &&
+                echo " ($((($(date +%s) - $(stat -c %Y flake.lock)) / 86400)) days ago)"
+            '';
+          };
+        }
+      );
+    };
+}
