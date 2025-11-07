3f45187065c9f3c5d61ecccf232a3f0cde4db2ef
Yusong <yusonglai64@gmail.com>
2025-11-07 09:22:40 +0800

    chore: quality of life improvements on scripts/validate.sh

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -37,25 +37,29 @@ kustomize_config="kustomization.yaml"
 kubeconform_flags=("-skip=Secret")
 kubeconform_config=("-strict" "-ignore-missing-schemas" "-schema-location" "default" "-schema-location" "/tmp/flux-crd-schemas" "-verbose")
 
-echo "INFO - Downloading Flux OpenAPI schemas"
+echo "INFO - # -------- Downloading Flux OpenAPI schemas -------- #"
 mkdir -p /tmp/flux-crd-schemas/master-standalone-strict
 curl -sL https://github.com/fluxcd/flux2/releases/latest/download/crd-schemas.tar.gz |
   tar zxf - -C /tmp/flux-crd-schemas/master-standalone-strict
+echo ""
 
+echo "INFO - # -------- Making sure yaml files are syntactically correct -------- #"
 find . -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file; do
   echo "INFO - Validating $file"
   yq e 'true' "$file" >/dev/null
 done
+echo ""
 
-echo "INFO - Validating base"
+echo "INFO - # -------- Validating ./base -------- #"
 find ./base -maxdepth 2 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file; do
   kubeconform "${kubeconform_flags[@]}" "${kubeconform_config[@]}" "${file}"
   if [[ ${PIPESTATUS[0]} != 0 ]]; then
     exit 1
   fi
 done
+echo ""
 
-echo "INFO - Validating kustomize overlays"
+echo "INFO - # -------- Validating kustomize overlays -------- #"
 find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file; do
   echo "INFO - Validating kustomization ${file/%$kustomize_config/}"
   kustomize build "${file/%$kustomize_config/}" "${kustomize_flags[@]}" |
@@ -64,3 +68,4 @@ find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' fil
     exit 1
   fi
 done
+echo ""
